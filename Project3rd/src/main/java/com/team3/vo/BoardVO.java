66a187e6fb3f9e2380ccc193a25e75d454a93b60
soldesk <soldesk@soldesk-PC>
2019-08-26 10:10:21 +0900

    commit

---
+++ Project3rd/src/main/java/com/team3/vo/BoardVO.java
@@ -0,0 +1,80 @@
+package com.team3.vo;
+
+import java.util.Date;
+
+public class BoardVO {
+	
+	private int articleNo;
+	private String articleType;
+	private String title;
+	private String contents;
+	private Date date;
+	private int hit;
+	private String writer;
+	private String commentNo;
+	
+	
+	public BoardVO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public int getArticleNo() {
+		return articleNo;
+	}
+	public void setArticleNo(int articleNo) {
+		this.articleNo = articleNo;
+	}
+	public String getArticleType() {
+		return articleType;
+	}
+	public void setArticleType(String articleType) {
+		this.articleType = articleType;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getCommentNo() {
+		return commentNo;
+	}
+	public void setCommentNo(String commentNo) {
+		this.commentNo = commentNo;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	
+	
+	
+}
